2c52e9165d0de709f0841a25e4c0a0218d03a0eb
yuexps <69074603+yuexps@users.noreply.github.com>
2026-03-06 21:22:31 +0800

    更换为更合适的"interval"

--- nonebot_plugin_course_schedule/__init__.py
+++ nonebot_plugin_course_schedule/__init__.py
@@ -49,8 +49,8 @@ from .utils.reminder import check_and_send_reminders
 
 scheduler.add_job(
     check_and_send_reminders,
-    trigger="cron",
-    minute=f"*/{config.course_reminder_interval}",
+    trigger="interval",
+    minutes=config.course_reminder_interval,
     id="course_schedule_reminder",
     replace_existing=True,
 )
